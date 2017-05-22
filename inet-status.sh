77e92c54e319ee9d5bd934b7ef77d6cce58bc2a3
sojit <sojithomas88@gmail.com>
2017-05-23 05:20:41 +0530

    Update inet-status.sh

--- inet-status.sh
+++ inet-status.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#This my initial try to check the status of eth0 interface of linux machine.
+#This my initial try to check the status of eth0 & set it up if its down.
+#
 ###End
 
 
 
-test="ping 192.168.1.1 -c 1"
+test="ping x.x.x.x -c 1"
 
 
-if  $test > /dev/null
+if  $test > /dev/null                   # if $test result exit 0 then it will echo its up...
  then
-    echo "The eth0 is UP"
- else
+    echo "The eth0 is UP"     
+ else                                   # if the $test exit status is 1 then it will ititate the interface command.
     ifup eth0 > /dev/null
     echo "Enabling eth0..."
 fi
+
+
