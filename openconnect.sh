f7c3773120b303275e78010ba90d8aa90de010a4
sojit <sojithomas88@gmail.com>
2017-05-25 02:44:41 +0530

    Create openconnect.sh

---
+++ openconnect.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Author=soji
+#version=1.0
+#This script use to make the vpn connections are bit easier.
+#Please be remember, I have saved the password in the "openpass" file, so that openconnect fetch it from the "--passwd-on-stdin".!
+
+open=openconnect 
+user=NAME 
+fre=vpn-link1 
+var=vpn-link2
+
+echo 'Please select the specific vpn
+1.link1
+2.link2'
+
+read -p "> " choise
+case $choise in
+
+         1) $open --user=$user --passwd-on-stdin<$HOME/Documents/scripts/fair/openpass $fre --juniper;;
+         2) $open --user=$user --passwd-on-stdin<$HOME/Documents/scripts/fair/openpass $var --juniper;;
+         *) echo "Please seclect the correct option" ;;
+ esac
+ 
+ #The END
+   
