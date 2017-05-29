6a942a5a2e2eb90986d5fe8cf772883cf65eb6e4
Soji <sojithomas88@gmail.com>
2017-05-30 04:49:10 +0530

    Create extractor1.sh

---
+++ extractor1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Author:Soji, version:1.3
+#The script is for dcrypting the gpg file.
+#END
+
+if [ $# -ne 1 ] #>/dev/null
+   then
+   echo "missing arguments??"
+   else
+      sbname=$1
+      if [ "$s2" != 'gpg' ]
+         then
+            echo "Oops wrong file"
+          else
+             echo "extracting..."
+              if echo $sbname | grep gpg
+                then
+                  read -p "Expecting a directory name:- " number
+                  echo "extracting..."
+                  mkdir $number > /dev/null
+                  gpg -o $number.tar.gz -d $sbname && tar -xvf $number.tar.gz -C $number >/dev/null && rm -vrf $number.tar.gz >/dev/null && cd $number/pl2-* > /dev/null && mv * ../ >/dev/null && cd ../ >/dev/null && rm -rf pl2-* >/dev/null
+                  echo "Completed!!"
+                  else
+                    :
+            fi
+    fi
+fi
