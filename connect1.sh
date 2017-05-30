66e6bcf88d64ee2b6c336a6f87950e7743814380
Soji <sojithomas88@gmail.com>
2017-05-31 01:39:49 +0530

    Update connect1.sh

--- connect1.sh
+++ connect1.sh
@@ -24,13 +24,12 @@ if [ "$#" -ne 1 ]
                 else
                    sshpass -p $pp ssh -p 42002 $up@x.x.x.1 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
              fi
-       elif [ $1 = pre2 ]
+      elif [ $1 = pre2 ]
           then
              p2="ping x.x.x.x2 -c 1"
              if $p2 >/dev/null
                 then
                    sshpass -p $pp ssh -p 42002 $up@x.x.x.x2 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
-
                  else
                    sshpass -p $pp ssh -p 42002 $up@x.x.x.2 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
              fi
