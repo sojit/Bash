991b39239e53b8e772d59e940255cec481b5ae35
Soji <sojithomas88@gmail.com>
2017-06-02 07:14:14 +0530

    Update configure.sh

--- Openconnect/configure.sh
+++ Openconnect/configure.sh
@@ -3,3 +3,17 @@
 #Version.1.2
 # This is the installer script.
 
+#!/bin/bash
+
+if [ ! -d $HOME/Opencon ]
+ then
+    mkdir -p $HOME/Opencon && touch $HOME/Opencon/pass && touch $HOME/Opencon/user
+      read -p "Please enter the username:" name
+         echo $name > $HOME/Opencon/user
+      read -p "Please enter the password:" password
+         echo $password > $HOME/Opencon/pass
+ else
+     :
+fi
+
+
