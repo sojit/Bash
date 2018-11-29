6d898f87c411fbc3055558437346c9a2ec558804
Soji <sojithomas88@gmail.com>
2018-11-29 08:46:18 +0530

    Update untar.sh

--- untar.sh
+++ untar.sh
@@ -1,1 +1,2 @@
 pl=plclient-*;for i in $pl; do t=`echo $i | cut -d- -f1-2`; mkdir $t; tar -xvf $i -C $t; cd $t; k=`echo *`; cd pl*; mv * ../; cd ../; sleep 1; rm -rf $k; cd ../; rm -rf $i; done
+grep -i 'v[0-9].[0-9].[0-9][0-9][0-9]/.*[^"*]' -o releases
