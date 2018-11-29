f5107268a2f0dc1d83b2e4bb86fe253b97324391
Soji <sojithomas88@gmail.com>
2018-11-29 08:49:54 +0530

    Update untar.sh

--- untar.sh
+++ untar.sh
@@ -1,2 +1,3 @@
 pl=plclient-*;for i in $pl; do t=`echo $i | cut -d- -f1-2`; mkdir $t; tar -xvf $i -C $t; cd $t; k=`echo *`; cd pl*; mv * ../; cd ../; sleep 1; rm -rf $k; cd ../; rm -rf $i; done
 grep -i 'v[0-9].[0-9].[0-9][0-9][0-9]/.*[^"*]' -o releases
+https://github.com/calmh/mole/releases
